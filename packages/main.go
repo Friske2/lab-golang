9b152ab9b72aba68f8c37f38c0ebb60b82e57f4f
dainokung <dainokung2535@gmail.com>
2019-08-11 01:12:29 +0700

    packages

---
+++ packages/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"lab-golang/packages/utils"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(math.Ceil(2.7))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(utils.Reverse("hello"))
+}
